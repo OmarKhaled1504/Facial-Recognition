e705b897559d9ecba6d8d6c3ab3c572e9974cc10
Omar <xxxomarxxx21@gmail.com>
2022-03-01 21:36:24 +0200

    read the dataset created D data matrix and y label vector

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import os
+import glob
 
-with open("/faces/s1/1.pgm", 'rb') as pgmf:
-    im = plt.imread(pgmf)
 
-print(im)
+#reading the pgm files and creating the data matrix
+D = []
+for i in range(1, 41):
+    for filepath in glob.glob(
+            os.path.join('C:/Users/Omar/PycharmProjects/facial recognition/faces/s{}'.format(i), '*.pgm')):
+        with open(filepath, 'rb') as f:
+            image = plt.imread(f).flatten()
+        image = list(image)
+        D.append(image)
+#print(D)
+#creating the y label vector
+y = []
+for i in range(1,41):
+    for j in range(1,11):
+        y.append(i)
+y = np.array([y]).T
+print(y)
+
